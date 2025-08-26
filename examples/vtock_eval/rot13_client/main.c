f2f8609a126516770ef897b67d14a63b8001c65f
Tyler Potyondy <potyondyt@gmail.com>
2025-08-26 11:56:07 -0700

    ipc pong: vtock eval
    - reorganize into vtock eval directory
    - move sensor app here

---
+++ examples/vtock_eval/rot13_client/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <libtock-sync/services/alarm.h>
+#include <libtock/kernel/ipc.h>
+
+#include <dwt.h>
+
+size_t rot13_svc_num = 0;
+
+char buf[64] __attribute__((aligned(64)));
+
+struct rot13_buf {
+  int8_t length;
+  char buf[31];
+};
+
+static void rot13_callback(__attribute__ ((unused)) int pid,
+                           __attribute__ ((unused)) int len,
+                           __attribute__ ((unused)) int arg2, void* ud) {
+  struct rot13_buf* rb = (struct rot13_buf*)ud;
+  uint32_t end = get_cycle_count();
+  printf("[EVAL] ipc pong %i\n", end);
+
+  printf("%d: %.*s\n", rb->length, rb->length, rb->buf);
+  libtocksync_alarm_delay_ms(500);
+  start_cycle_counter();
+  ipc_notify_service(rot13_svc_num);
+}
+
+int main(void) {
+  int err;
+  err = ipc_discover("org.tockos.examples.rot13", &rot13_svc_num);
+  if (err < 0) {
+    printf("No rot13 service\n");
+    return -1;
+  }
+
+  struct rot13_buf* rb = (struct rot13_buf*)buf;
+  ipc_register_client_callback(rot13_svc_num, rot13_callback, rb);
+
+  rb->length = snprintf(rb->buf, sizeof(rb->buf), "Hello World!");
+  ipc_share(rot13_svc_num, rb, 64);
+
+  ipc_notify_service(rot13_svc_num);
+
+  while (1) {
+    yield();
+  }
+}
